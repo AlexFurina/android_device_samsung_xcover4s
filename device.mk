e72884ed7b8a57f5155bc9ef703f0fcf079ebc19
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-12-31 15:43:05 +0200

    xcover4s: device.mk: remove redundant fastboot line
    * Not needed for twrp-12.1
    
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -3,8 +3,7 @@ LOCAL_PATH := device/samsung/xcover4s
 # Fastbootd
 PRODUCT_PACKAGES += \
 	fastbootd \
-	android.hardware.fastboot@1.0-impl-mock \
-	android.hardware.fastboot@1.0-impl-mock.recovery
+	android.hardware.fastboot@1.0-impl-mock
 
 PRODUCT_PRODUCT_PROPERTIES += \
 	ro.fastbootd.available=true
