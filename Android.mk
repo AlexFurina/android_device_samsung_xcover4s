1c9212f8f2a734cf5c11eccd4b4b83dd4fb5fa07
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2025-10-30 07:55:40 +0900

    xcover4s: treewide: update license

--- Android.mk
+++ Android.mk
@@ -1,3 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 ifneq ($(filter xcover4s, $(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 The TWRP Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/twrp_xcover4s.mk
 
